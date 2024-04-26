fd15751ac181e624188e47fb22c2568f8f390098
marlshane <marlshaneesteron2302@gmail.com>
2024-04-26 13:53:29 +0800

    implement multiplication table feature

---
+++ assignment.rb
@@ -0,0 +1,16 @@
+#Assignment
+
+(1..10).each do |x|
+        print "#{x*y} "
+    end
+    puts
+end
+
+print "\n"
+
+for x in 1..10
+    for y in 1..10
+        print "#{x*y} "
+    end
+    puts
+end
