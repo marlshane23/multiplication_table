d49421bd8aca5ea507bb897d0778c810f66d1af6
marlshane <marlshaneesteron2302@gmail.com>
2024-04-26 19:58:09 +0800

    testing git

---
+++ test.rb
@@ -0,0 +1,11 @@
+puts "=" * 50
+puts "---------------MULTIPLICATION TABLE---------------"
+puts "=" * 50
+(1..10).each do |x|
+    (1..10).each do |y|
+         print "#{x*y} | "
+end
+    puts
+    puts "-" * 50
+
+end
